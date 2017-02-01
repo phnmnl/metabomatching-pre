472786037a4bcbcb5f511d4e4d42ab43222e7257
Rico Rueedi <metabomatching@unil.ch>
2017-02-01 15:11:54 +0100

    Update -test.sh.

--- Dockerfile
+++ Dockerfile
@@ -15,7 +15,7 @@ RUN apt-get install -y \
 # Clean up
 RUN apt-get -y clean && apt-get -y autoremove && rm -rf /var/lib/{cache,log}/ /tmp/* /var/tmp/*
 
-ENV TOOL_VERSION=0.1.10
+ENV TOOL_VERSION=0.1.11
 ENV CONTAINER_VERSION=0.2.1.4
 
 # Install metabomatching
--- metabomatching-test.sh
+++ metabomatching-test.sh
@@ -19,6 +19,6 @@ metabomatching.sh -g "$PROG_DIR_NAME/test" || exit 1
 # Test with an input file
 scores_file=$PROG_DIR_NAME/test/sco.tsv
 pdf_file=$PROG_DIR_NAME/test/sco.pdf
-metabomatching.sh -g -i "$PROG_DIR_NAME/test/ps.test.multi/test.pseudospectrum.tsv" -s "$scores_file" -S "$pdf_file" || exit 1
+metabomatching.sh -g -i "$PROG_DIR_NAME/test/ps.test.multi/test.pseudospectrum.tsv" -p "$PROG_DIR_NAME/test/parameters.gxy.1.tsv" -s "$scores_file" -S "$pdf_file" || exit 1
 [ -f "$scores_file" ] || exit 1
 [ -f "$svg_file" ] || exit 1
